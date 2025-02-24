9c488bba49ba00150bfa468c1a9ab1a54b09af93
Rahul1122006 <rahul92a6569@gmail.com>
2025-02-24 15:14:12 +0530

    Solution to Check MSB of a Number

--- Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
-
 int main() {
-    int a;
-    scanf("%d",&a);
-    (a>>31 == 0)?printf("Not Set"):printf("Set")
-    (a>>31 == 0)?printf("Not Set"):printf("Set");
-}
+    int number, n, bit_value;
+        
+            // Reading two integers: the number and the bit position (0-based)
+                scanf("%d %d", &number, &n);
+                    
+                        // Retrieve the nth bit using bitwise shift and bitwise AND
+                            bit_value = (number >> n) & 1;
+                                
+                                    // Output the value of the nth bit (0 or 1)
+                                        printf("%d\n", bit_value);
+                                            
+                                                return 0;
+                                                }
